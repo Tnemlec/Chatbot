4b97770776e08eb95407d96f6632a66182e35f51
Aursen <jeanno11@live.fr>
2021-03-16 15:33:46 +0100

    :star: Add new classes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
-import express = require("express");
-import lastfm from "./lastfm/index";
-import bodyparser from 'body-parser';
+import express from 'express';
+import lastfm from './lastfm/index';
 import FBManager from './FBManager/index';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 const server = express();
 const PORT = 3000;
@@ -17,7 +16,8 @@ if(server.settings.env === "development"){
 //let fb_client = new FBManager();
 let api_client = new lastfm();
 
-server.get('/', (req, res) => {
+server.get('/', async (req, res) => {
+    res.send(await api_client.get_top_3("cher"));
 });
 
 server.listen(PORT, () => {
--- src/lastfm/index.ts
+++ src/lastfm/index.ts
@@ -4,18 +4,22 @@ export default class LastFm {
     apiKey: string;
 
     constructor() {
-        this.apiKey = process.env.LASTFM_KEY || "";
-    }
-    
-    public get_track_info(name: string, artist: string): string {
-        this.make_request("track.getInfo", { artist: artist, track: name });
-        return "";
+        this.apiKey = process.env.LASTFM_KEY ?? '';
     }
 
-    make_request(method: string, parameters: object): any {
-        axios.get(`http://ws.audioscrobbler.com/2.0/?method=${method}&api_key=${this.apiKey}&format=json`, { params: parameters }).then( (res) => {
-            return res.data;
-        }
-        ).catch(console.log);
+    public async get_top_3(artiste_name: string): Promise<string> {
+        let data = await this.make_request('artist.gettoptracks', {artist: artiste_name, limit: 5});
+        
+        return JSON.stringify(data);
+    }
+
+    public async get_track_info(name: string, artist: string): Promise<string> {
+        let data = await this.make_request('track.getInfo', { artist: artist, track: name });
+        return JSON.stringify(data);
+    }
+
+    private async make_request(method: string, parameters: object): Promise<object> {
+        let resp = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=${method}&api_key=${this.apiKey}&format=json`, { params: parameters });
+        return resp.data;
     }
 }
