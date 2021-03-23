6ad54a064e4bbfabcb651e34264def3266fc3c22
Aursen <jeanno11@live.fr>
2021-03-23 17:46:54 +0100

    :start: Add new patterns

--- src/Regex/index.ts
+++ src/Regex/index.ts
@@ -4,77 +4,92 @@ import xregexp from 'xregexp';
 import LastFm from '../LastFM/index';
 import Track from '../LastFM/track';
 
-export default class PatternHandler{
+export default class PatternHandler {
     api_client: LastFm;
 
-    constructor(api_client: LastFm){
+    constructor(api_client: LastFm) {
         this.api_client = api_client;
     }
 
-    async matchPattern(message: any){
+    async matchPattern(message: any) {
         let found = pattern.find(item => {
-            if(xregexp.test(message.text, xregexp(item.pattern, 'i'))){
-                return true
+            if (xregexp.test(message.text, xregexp(item.pattern, 'i'))) {
+                return true;
             }
         })
-        if(found){
-            return await this.getResponse(found.intent, this.createEntities(message.text, found.pattern))
+        if (found) {
+            return await this.getResponse(found.intent, this.createEntities(message.text, found.pattern));
         }
-        else{
-            return "Sorry I didn't understand 🤷‍♂️"
+        else {
+            return "Sorry I didn't understand 🤷‍♂️";
         }
     }
 
-    createEntities(message: string, pattern: any){
-        return xregexp.exec(message , xregexp(pattern,"i"))
+    createEntities(message: string, pattern: any) {
+        return xregexp.exec(message, xregexp(pattern, "i"));
     }
 
-    async getResponse(intent: string, entities: any){
+    async getResponse(intent: string, entities: any) {
+        let number: number;
+        let name: string;
+
         switch (intent) {
             case "salutation":
-                return 'Hey 👋'
-                break;
-            
+                return 'Hey 👋';
+
             case "exit":
-                return 'See you later ! 🖐'
-                break;
+                return 'See you later ! 🖐';
 
             case "get_n_top_songs":
-                let answer = ''
+                let answer: string = '';
                 let top_n = null;
                 let tracks: Track[];
-                if(entities[1]){
-                    top_n = parseInt(entities[1])
+                if (entities[1]) {
+                    top_n = parseInt(entities[1]);
                 }
                 let artist_name = entities[2]
                 artist_name = artist_name.trim()
-                if(top_n != 0 && top_n != null){
+                if (top_n != 0 && top_n != null) {
                     tracks = await this.api_client.get_top(artist_name, top_n);
                 }
-                else{
+                else {
                     tracks = await this.api_client.get_top(artist_name);
                 }
                 let result: string = "";
 
-                let i = 0
+                let i: number = 0;
                 tracks.forEach((track: Track) => {
-                    i+=1
+                    i += 1
                     result += `${i} - ${track.name}\n`;
                 });
 
-                if(top_n != 0 && top_n != null){
-                    answer = `Here are the top ${top_n} songs from ${artist_name}\n${result}`
-                }
-                else{
-                    answer = `Here are the top 3 songs from ${artist_name}\n${result}`
-                }
+                answer = `Here are the top ${top_n != 0 && top_n != null ? top_n : 3} songs from ${artist_name}\n${result}`;
 
-                return answer
-                break;
-        
+                return answer;
+            case "search_songs":
+                number = entities.number ?? 3;
+                name = entities.name;
+                if (name)
+                    return "Sorry, but the name is empty 🤷‍♂️";
+
+                const artists: string[] = await this.api_client.search_artists(name, number);
+                artists.map((value, index) => `${index}.${value}`);
+
+                return `Here are the ${number} artists searched:\n${artists.join('\n')}`;
+
+            case "search_artists":
+                number = entities.number ?? 3;
+                name = entities.name;
+                if (name)
+                    return "Sorry, but the name is empty 🤷‍♂️";
+
+                const songs: Track[] = await this.api_client.search_tracks(name, number);
+                songs.map((value, index) => `${index}.${value}`);
+
+                return `Here are the ${number} songs searched:\n${songs.join('\n')}`;
+
             default:
-                return "Sorry I didn't understand 🤷‍♂️"
-                break;
+                return "Sorry I didn't understand 🤷‍♂️";
         }
     }
 }
--- src/Regex/pattern.ts
+++ src/Regex/pattern.ts
@@ -1,13 +1,19 @@
 const patternDict = [{
-    pattern:'\\b(?<greeting>Hi|Hello|Hey)\\b',
+    pattern: '\\b(?<greeting>Hi|Hello|Hey)\\b',
     intent: 'salutation'
-    },{
-    pattern:'\\b(bye|exit)\\b',
+}, {
+    pattern: '\\b(bye|exit)\\b',
     intent: 'exit'
-    },{
+}, {
     pattern: '([0-9])? best songs from ([^?]*) ?',
     intent: 'get_n_top_songs'
-    }
+},{
+    pattern: '(?<number>\\d*).*(tracks?|songs?|musics?).*(with|contains?)\\s(?<name>.*)',
+    intent: 'search_songs'
+}, {
+    pattern: '(?<number>\\d*).*(creators?|artists?).*(with|contains?)\\s(?<name>.*)',
+    intent: 'search_artists'
+}
 ]
 
 export default patternDict;
